cea8bcc0a9cee91258115686f4e7f752f50afb33
Léo <leo@barkibu.com>
2021-01-14 09:36:14 +0100

    Optimize / Generalize and Test Uniqueness Validator

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,13 @@
 require 'pathname'
 require 'bundler/setup'
+require 'sinatra'
+require 'webmock/rspec'
 require 'kb'
 
 gem_root = Pathname.new('..').expand_path(File.dirname(__FILE__))
 Dir[gem_root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+
+ENV['KB_API_URL_TEMPLATE'] = 'https://test.api.%<bounded_context>s.test_api_barkkb.com/%<version>s/%<entity>s'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
