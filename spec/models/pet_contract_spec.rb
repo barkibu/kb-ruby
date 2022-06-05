b211a89fd9f252264c210f082bfbee2ebffe449f
Juan F. Pérez <761794+jguitar@users.noreply.github.com>
2022-06-05 10:28:50 +0200

    move setup data to proper test

--- spec/models/pet_contract_spec.rb
+++ spec/models/pet_contract_spec.rb
@@ -1,26 +1,25 @@
 require 'spec_helper'
 
 RSpec.describe KB::PetContract do
-  before do
-    client = described_class.send(:kb_client)
-    connection = client.send('connection')
-    connection.builder.adapter :test, stubs
-  end
-
-  after { described_class.send(:remove_instance_variable, :@kb_client) }
-
-  let(:stubs) do
-    Faraday::Adapter::Test::Stubs.new
-  end
-  let(:contract_number) { 'MyContractNumber' }
-  let(:resource_path) { "/v1/petcontracts/contractnumber/#{contract_number}" }
-  let(:contract_resource) do
-    { 'key' => 'contract_key', 'contract_number' => contract_number, 'plan_key' => 'plan_key' }
-  end
-
-  describe '#find_by_contract_number' do
+  describe '.find_by_contract_number' do
     subject(:find_by_contract_number) { described_class.find_by_contract_number(contract_number) }
 
+    before do
+      client = described_class.send(:kb_client)
+      connection = client.send('connection')
+      connection.builder.adapter :test, stubs
+    end
+
+    after { described_class.send(:remove_instance_variable, :@kb_client) }
+
+    let(:stubs) do
+      Faraday::Adapter::Test::Stubs.new
+    end
+    let(:contract_number) { 'MyContractNumber' }
+    let(:contract_resource) do
+      { 'key' => 'contract_key', 'contract_number' => contract_number, 'plan_key' => 'plan_key' }
+    end
+    let(:resource_path) { "/v1/petcontracts/contractnumber/#{contract_number}" }
     let(:api_response) { [200, { 'Content-Type': 'application/json' }, contract_resource.to_json] }
 
     it 'passes locale as params' do
