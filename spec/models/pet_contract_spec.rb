1d4771cb83a4babbdd3520f4944acd46e78bdc30
Edu Saborit <esaborit4code@users.noreply.github.com>
2023-09-15 17:53:00 +0200

    Fix test using stub_request instead of Faraday test adapter

--- spec/models/pet_contract_spec.rb
+++ spec/models/pet_contract_spec.rb
@@ -5,28 +5,17 @@ RSpec.describe KB::PetContract do
     subject(:find_by_contract_number) { described_class.find_by_contract_number(contract_number) }
 
     before do
-      client = described_class.send(:kb_client)
-      connection = client.send('connection')
-      connection.builder.adapter :test, stubs
+      stub_request(:get, "https://test_api_barkkb.com/v1/petcontracts/contractnumber/#{contract_number}")
+        .to_return(status: 200, body: contract_resource.to_json)
     end
 
-    after { described_class.send(:remove_instance_variable, :@kb_client) }
-
-    let(:stubs) do
-      Faraday::Adapter::Test::Stubs.new
-    end
     let(:contract_number) { 'MyContractNumber' }
     let(:contract_resource) do
       { 'key' => 'contract_key', 'contract_number' => contract_number, 'plan_key' => 'plan_key' }
     end
-    let(:resource_path) { "/v1/petcontracts/contractnumber/#{contract_number}" }
-    let(:api_response) { [200, { 'Content-Type': 'application/json' }, contract_resource.to_json] }
 
-    it 'passes locale as params' do
-      stubs.public_send(:get, resource_path) { api_response }
-
+    it 'returns a KB::PetContract' do
       expect(find_by_contract_number).to be_a described_class
-      stubs.verify_stubbed_calls
     end
   end
 
